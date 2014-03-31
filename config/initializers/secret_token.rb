a0cfb7aa691fcd00f704ce02ba5ea3d89325e1ac
Matt Freeman <rmf@rmfs-MacBook-Pro.local>
2014-03-30 20:55:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Humon::Application.config.secret_key_base = '56a3c10d39eba33d813aa412253f5b42734eae03d32a30c196fbbd3930271f6dd5cf186621625348960fc299e7b17a1fa7adf2b9c2a84344dac0f3cf3f486daf'
